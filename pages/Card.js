eb5140e13a7097bae13a1cbbd3817e5a60f1ba79
vsemoth <vsemoth@gmail.com>
2021-08-11 20:36:38 +0200

    Fixed CSS issue

--- pages/Card.js
+++ pages/Card.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Card = ({ id }) => {
   return (
     <Image
-      className={styles.image}
+      className={styles.image, styles.card}
       src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`}
       width={100}
       height={100}
